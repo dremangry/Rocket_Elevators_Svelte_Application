a7cadf751d20ecd20d5c911a1972a5db2eec7732
dremangry <andrezreyes2015@gmail.com>
2022-05-08 20:11:10 -0400

    last commit

---
+++ src/components/nav.svelte
@@ -0,0 +1,57 @@
+<nav>
+    
+    <ul>
+        <li>
+            <a class="navbar-brand" href="/">
+                <div class="logo-image">
+                    <img src="src/img/logo_Rocket.png" class="img-fluid">
+                </div>
+            </a>
+        </li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/connect">Connect your wallet</a></li>
+        <li><a href="/portofolio">Your portfolio</a></li>
+    </ul>
+</nav>
+
+<style>
+
+    .logo-image{
+    width: 120px;
+    /* height: 100vh; */
+    /* border-radius: 50%; */
+    overflow: hidden;
+    margin-top: -6px;
+    margin-bottom: -8px;
+    }
+
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+    }
+    
+    li {
+      float: left;
+    }
+    
+    li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+    
+    li a:hover {
+      background-color: #111;
+      /* color: aqua; */
+    }
+    </style>
+
+
+
+  
+
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Nav from "../components/nav.svelte";
+  // import Footer from "../components/footer.svelte";
+  import "../app.scss";
+</script>
+<Nav/>
+
+<slot></slot>
+<!-- <Footer/> -->
